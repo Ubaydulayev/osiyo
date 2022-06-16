71c7c0685c356453de862fa55bcbb3f7c43c2626
Ubaydulayev <ubaydullayevdastonbek@gmail.com>
2022-06-16 13:21:36 +0500

    new components and jivochat

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import Home from "../../pages/Home/Home";
+import Home from "../../pages/home/Home";
 import Header from "../../container/header/Header";
 import Footer from "../../container/footer/Footer";
 
